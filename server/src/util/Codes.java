303f5d7dcfcd5727287b4ec1a811f9ba8121208d
bencroskery <ben@irtech.com>
2017-02-23 22:27:01 -0500

    Initial Jave OPCodes
    - not used anywhere yet, will be changed/added to

---
+++ server/src/util/Codes.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Codes {
+	
+	public static final byte SERVER = 0;
+	public static final byte APP    = 1;
+	public static final byte DEVICE = 2;
+	
+	public static final byte BEAT   = 0;
+	public static final byte ACK    = 1;
+	public static final byte INPUT  = 2;
+	public static final byte OUTPUT = 3;
+	public static final byte CONF   = 4;
+
+}
