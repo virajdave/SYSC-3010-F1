f32abf123aba84ecca4b7ed580a4589258f6cb1b
bencroskery <ben@irtech.com>
2017-02-23 23:56:50 -0500

    Manager message types + device connection
    - Added some skeleton code for message types and what to do with them
    - Got some simple code working to create a device connection (need
    drivers)
    - Switched OPCodes to chars since it's easier that way

--- server/src/Manager.java
+++ server/src/Manager.java
@@ -1,4 +1,5 @@
 import types.*;
+import util.Codes;
 
 public class Manager extends Thread {
 	Web web;
@@ -17,15 +18,99 @@ public class Manager extends Thread {
     		// Check for new incoming messages.
         	Message msg = server.recvWait();
         	if (msg != null) {
-        		Integer d = web.get(msg.getSocketAddress());
-        		if (d != null) {
-    				System.out.println("This device exists.");
-        		} else {
-        			web.add(msg.getSocketAddress());
-    				System.out.println("Added new device.");
+        		char who = msg.getMessage().charAt(0);
+        		switch (who) {
+        			case Codes.W_DEVICE:
+        				device(msg); break;
+        			case Codes.W_APP: 
+        				app(msg); break;
+        			default:
+        				System.out.println("wat is dis -> " + msg.toString());
         		}
         	}
     	}
+	}
+	
+	private void device(Message msg) {
+		char code = msg.getMessage().charAt(1);
+		Integer id = web.get(msg.getSocketAddress());
+		if (id == null) {
+			if (code == Codes.T_ACK) {
+				String[] info = msg.getMessage().substring(2).split("/");
+				int type = -1;
+				try {
+					type = Integer.parseInt(info[1]);
+				} catch (NumberFormatException e) {
+					System.out.println("Device type '" + info[1] + "' unknown.");
+					return;
+				} catch (Exception e) {
+					System.out.println("Couldn't parse info.");
+					return;
+				}
+				// Create device blah blah
+				System.out.println("Device of type: " + type);
+				
+				// Add id mapping
+				id = web.add(msg.getSocketAddress());
+				System.out.println("Added device #" + id);
+				// Send ack back letting the device know it was connected.
+				server.sendMessage(new Message("" + Codes.W_SERVER + Codes.T_ACK, msg.getSocketAddress()));
+			} else {
+				// Don't know what this is, looking for info with a beat.
+				server.sendMessage(new Message("" + Codes.W_SERVER + Codes.T_BEAT, msg.getSocketAddress()));
+			}
+			return;
+		}
+		System.out.println("Message from device #" + id + ": "  + msg.getMessage());
+		
+		// Quick check device ID matches.
+		
+		switch (code) {
+			case Codes.T_BEAT:
+				// uh don't know what to do here.
+				break;
+			case Codes.T_ACK:
+				// Check if anything is waiting on an ack?
+				break;
+			case Codes.T_INPUT:
+				// Look through ruleset to see if anything expects this input.
+				// Send to device driver.
+				break;
+			case Codes.T_OUTPUT: 
+				// Probably asking for an output? Not sure if this happens.
+				break;
+			case Codes.T_CONF: 
+				// This also probably shouldn't happen?
+				break;
+			default:
+				System.out.println("wat is dis -> " + msg.toString());
+		}
+	}
+	
+	private void app(Message msg) {
+		char code = msg.getMessage().charAt(1);
+		
+		switch (code) {
+			case Codes.T_BEAT:
+				// Send back an ack.
+				server.sendMessage(new Message("" + Codes.W_SERVER + Codes.T_ACK, msg.getSocketAddress()));
+				break;
+			case Codes.T_ACK:
+				// Check if anything is waiting on an ack?
+				break;
+			case Codes.T_INPUT:
+				// Look through ruleset to see if anything expects this input.
+				// Send to device driver.
+				break;
+			case Codes.T_OUTPUT: 
+				// Probably asking for an output? Not sure if this happens.
+				break;
+			case Codes.T_CONF: 
+				// This also probably shouldn't happen?
+				break;
+			default:
+				System.out.println("wat is dis -> " + msg.toString());
+		}
 	}
 	
 	public static void main(String[] args) {
--- server/src/util/Codes.java
+++ server/src/util/Codes.java
@@ -2,14 +2,14 @@ package util;
 
 public class Codes {
 	
-	public static final byte SERVER = 0;
-	public static final byte APP    = 1;
-	public static final byte DEVICE = 2;
+	public static final char W_SERVER = '0';
+	public static final char W_APP    = '1';
+	public static final char W_DEVICE = '2';
 	
-	public static final byte BEAT   = 0;
-	public static final byte ACK    = 1;
-	public static final byte INPUT  = 2;
-	public static final byte OUTPUT = 3;
-	public static final byte CONF   = 4;
+	public static final char T_BEAT   = '0';
+	public static final char T_ACK    = '1';
+	public static final char T_INPUT  = '2';
+	public static final char T_OUTPUT = '3';
+	public static final char T_CONF   = '4';
 
 }
