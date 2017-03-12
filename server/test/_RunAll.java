9cf8aa8e87b79d62a353470bae29e1cc3d12a8e3
bencroskery <ben@irtech.com>
2017-03-12 10:42:36 -0400

    A commit

--- server/test/_RunAll.java
+++ server/test/_RunAll.java
@@ -1,5 +1,6 @@
 import org.junit.extensions.cpsuite.ClasspathSuite;
 import org.junit.runner.RunWith;
 
+// This will run all tests in the project classpath.
 @RunWith(ClasspathSuite.class)
 public class _RunAll {}
