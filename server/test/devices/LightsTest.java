4fe90e6685cdb68e098ed74c5735406b286fa57f
bencroskery <ben@irtech.com>
2017-03-12 11:33:47 -0400

    Added Info tests

--- server/test/devices/LightsTest.java
+++ server/test/devices/LightsTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import types.Data;
+
 public class LightsTest {
 
 	@Test
@@ -14,6 +16,20 @@ public class LightsTest {
 		assertEquals(5, test.getID());
 		assertTrue(test.hasID(5));
 		assertFalse(test.hasID(96));
+	}
+	
+	@Test
+	public void testInfo() {
+		Device test = new Lights();
+		assertEquals("-1/0/0", test.getInfo());
+		
+
+		test = Device.createNew(0, 5, null);
+		assertEquals("5/0/0", test.getInfo());
+		test.setDead(true);
+		assertEquals("5/1/0", test.getInfo());
+		test.giveInput(new Data("set", "1"));
+		assertEquals("5/1/1", test.getInfo());
 	}
 
 }
--- server/test/devices/SwitchTest.java
+++ server/test/devices/SwitchTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import types.Data;
+
 public class SwitchTest {
 
 	@Test
@@ -15,4 +17,18 @@ public class SwitchTest {
 		assertTrue(test.hasID(0));
 		assertFalse(test.hasID(50));
 	}
+	
+	@Test
+	public void testInfo() {
+		Device test = new Switch();
+		assertEquals("-1/0/0/-1", test.getInfo());
+		
+		test = Device.createNew(1, 5, null);
+		assertEquals("5/0/0/-1", test.getInfo());
+		test.setDead(true);
+		assertEquals("5/1/0/-1", test.getInfo());
+		test.giveInput(new Data("set", "1"));
+		assertEquals("5/1/1/-1", test.getInfo());
+	}
+	
 }
