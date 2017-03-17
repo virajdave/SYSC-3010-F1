c5ccdf5ae10a2acf331062854c4e856579b90e2b
connoremery <connoremery@mc6045-06.labs.sce.carleton.ca>
2017-03-17 10:53:42 -0400

    Thermostat driver

--- server/src/devices/Thermostat.java
+++ server/src/devices/Thermostat.java
@@ -1,28 +1,37 @@
 package devices;
 
 import types.Data;
+import util.Parse;
 
 /**
  * Created by Mega Prime 2.0 on 3/16/2017.
  */
 public class Thermostat extends Device {
-    @Override
+	int thermostatTemp = -500;
+    @Override // input from device to driver 
     public void giveMessage(String msg) {
-
+    	thermostatTemp = Parse.toInt(msg);
     }
 
-    @Override
+    @Override //input to device
     public void giveInput(Data in) {
-
+    	int temp =  Parse.toInt(in.get());
+    	send(in.get());
     }
 
-    @Override
+    @Override // for other drivers requests
     public Data requestOutput(Data in) {
-        return null;
+    	if (thermostatTemp != -500)
+        return new Data("temp", Parse.toString(this.thermostatTemp));
+    	//if no data available
+        return new Data("temp", "no temp ");
     }
 
-    @Override
+    @Override //for app requests
     public String getInfo() {
-        return null;
+    	if (thermostatTemp != -500)
+            return  Parse.toString(this.thermostatTemp);
+        	//if no data available
+            return "no temp ";
     }
 }
