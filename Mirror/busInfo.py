830804f03a0f3f511af056d2a5bd771d42fb483c
Dillon Verhaeghe <nollid30@hotmail.com>
2017-03-26 12:48:51 -0400

    made cleaner way to get collect trip info from bussing

--- Mirror/busInfo.py
+++ Mirror/busInfo.py
@@ -29,8 +29,8 @@ def parseBusInfo(info):
 		data['direction' + str(directionNum)] = dict(
 				dest = str(route['RouteNo']) + ': ' + route['RouteLabel']
 		)
-		if len(trips) > 0 and type(trips.get('Trip')) is list:
-			for x in range(0,len(trips)):
+		if type(trips.get('Trip')) is list:
+			for x in range(0,len(trips.get('Trip')) ):
 				data['direction' + str(directionNum)]['busTime' + str(x)] = trips.get('Trip')[x].get('AdjustedScheduleTime') + ' mins'
 		elif  len(trips) > 0:
 			data['direction' + str(directionNum)]['busTime0'] = trips.get('Trip').get('AdjustedScheduleTime') + ' mins'
