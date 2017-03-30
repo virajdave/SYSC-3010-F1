907cee5e7d576c58a07745aaa86942604f9d4a15
Dillon Verhaeghe <nollid30@hotmail.com>
2017-03-29 21:49:18 -0400

    changes after testing with the server to get the mirror working with what the server expects

--- Mirror/mirrorController.py
+++ Mirror/mirrorController.py
@@ -7,7 +7,7 @@ from queue import *
 import _thread, time
 
 #device id
-id = -1
+id = '-1'
 
 # Queue setup
 guiRecvQueue = Queue()
@@ -16,75 +16,81 @@ recvQueue = Queue()
 
 # Continuly runs to make sure the gui updates time and date independent of everything else
 def tellGUIToUpdateTime():
-	while True :
-		hour = str(int(strftime("%I")))
-		minute = strftime("%M")
-		curTime = hour + ':' + minute
-		guiRecvQueue.put_nowait(message('time', curTime))
-		
-		date = strftime("%A, %B %d %Y")
-		guiRecvQueue.put_nowait(message('date', date))
-		time.sleep(0.5)
+    while True :
+        hour = str(int(strftime("%I")))
+        minute = strftime("%M")
+        curTime = hour + ':' + minute
+        guiRecvQueue.put_nowait(message('time', curTime))
+        
+        date = strftime("%A, %B %d %Y")
+        guiRecvQueue.put_nowait(message('date', date))
+        time.sleep(0.5)
 
 # Every 15 mins fetches new weather data and gives the info to the gui to display
 def tellGUIToUpdateWeather():
-	while True :
-		#weatherData = data_organizer(data_fetch(url_builder('Ottawa,Ca')))
-		#guiRecvQueue.put_nowait(message('weather', weatherData))
-		sendQueue.put_nowait(message('data', 'weather'))
-		time.sleep(900)
+        global id
+        while True :
+        #weatherData = data_organizer(data_fetch(url_builder('Ottawa,Ca')))
+        #guiRecvQueue.put_nowait(message('weather', weatherData))
+                if (id != '-1'):
+                        sendQueue.put_nowait(message('data', id + '/weather'))
+                time.sleep(900)
 
 def tellGUIToUpdateBusInfo():
-	while True:
-		#busInfo = parseBusInfo(getBusInfo(urlBuilderBus(3031, 104)))
-		#guiRecvQueue.put_nowait(message('bus', busInfo))
-		sendQueue.put_nowait(message('data', 'bus'))
-		time.sleep(30)
-	
+        global id
+        while True:
+        #busInfo = parseBusInfo(getBusInfo(urlBuilderBus(3031, 104)))
+        #guiRecvQueue.put_nowait(message('bus', busInfo))
+                if (id != '-1'):
+                        sendQueue.put_nowait(message('data', id + '/bus'))
+                time.sleep(30)
+    
 
-def watchRecvMessages()	:
-	while True:
-			if not recvQueue.empty():
-				while not recvQueue.empty():
-					messageRecv = recvQueue.get()
-					if (messageRecv.type == 'data'):
-						if (messageRecv.info[0] == 'w'):
-							weatherInfo = data_organizer(messageRecv.info[1:])
-							guiRecvQueue.put_nowait(message('weather', weatherInfo))
-						if (messageRecv.info[0] == 'b'):
-							busInfo = parseBusInfo(messageRecv.info[1:])
-							guiRecvQueue.put_nowait(message('bus', busInfo))
-						if (messageRecv.info[0] == 'c'):
-							guiRecvQueue.put_nowait(message('colour', messageRecv.info[1:]))
-						if (messageRecv.info[0] == 't'):
-							#TO-DO add changing system time
-					elif(messageRecv.type == 'id'):
-							id = messageRecv.info
-					elif(messageRecv.type == 'beat'):
-							sendQueue.put_nowait(message('beat', id))
-			time.sleep(0.001)
-			
-			
+def watchRecvMessages():
+        global id
+        while True:
+                if not recvQueue.empty():
+                        while not recvQueue.empty():
+                                messageRecv = recvQueue.get()
+                                if (messageRecv.messageType == 'data'):
+                                        if (messageRecv.info[0] == 'w'):
+                                                weatherInfo = data_organizer(messageRecv.info[1:])
+                                                guiRecvQueue.put_nowait(message('weather', weatherInfo))
+                                        if (messageRecv.info[0] == 'b'):
+                                                busInfo = parseBusInfo(messageRecv.info[1:])
+                                                guiRecvQueue.put_nowait(message('bus', busInfo))
+                                        if (messageRecv.info[0] == 'c'):
+                                                guiRecvQueue.put_nowait(message('colour', messageRecv.info[1:]))
+                                elif(messageRecv.messageType == 'id'):
+                                        if (id == '-1'):
+                                                id = messageRecv.info
+                                                print('id: ' + id)
+                                elif(messageRecv.messageType == 'beat'):
+                                        print('thing:' + id)
+                                        sendQueue.put_nowait(message('beat', id))
+                        time.sleep(0.001)
+            
+            
 #Starts up the network, controller and gui threads
 def runController():
-	top = Tk() 	#used as the root for the tk window
-	#Start up threads 
-	gui = mirrorGUI(top, guiRecvQueue)
-	_thread.start_new_thread(gui.runnerLoop, (guiRecvQueue,))
-	_thread.start_new_thread(tellGUIToUpdateTime, ())
-	_thread.start_new_thread(tellGUIToUpdateWeather, ())
-	_thread.start_new_thread(tellGUIToUpdateBusInfo, ())
-	
-	#_thread.start_new_thread(mirrorNetRecv, (recvQueue,8080,))
-	_thread.start_new_thread(watchRecvMessages, ())
-	_thread.start_new_thread(networkInit, (recvQueue,sendQueue,))
-	#time.sleep(1)
-	#_thread.start_new_thread(sendFakeData, ())
-	
-	
-	#Display the gui window
-	gui.showGUI()
-	
-	
-	
+    top = Tk()     #used as the root for the tk window
+    #Start up threads 
+    gui = mirrorGUI(top, guiRecvQueue)
+    _thread.start_new_thread(gui.runnerLoop, (guiRecvQueue,))
+    _thread.start_new_thread(tellGUIToUpdateTime, ())
+    _thread.start_new_thread(tellGUIToUpdateWeather, ())
+    _thread.start_new_thread(tellGUIToUpdateBusInfo, ())
+    
+    #_thread.start_new_thread(mirrorNetRecv, (recvQueue,8080,))
+    _thread.start_new_thread(watchRecvMessages, ())
+    _thread.start_new_thread(networkInit, (recvQueue,sendQueue,))
+    #time.sleep(1)
+    #_thread.start_new_thread(sendFakeData, ())
+    
+    
+    #Display the gui window
+    gui.showGUI()
+    
+    
+    
 runController()
--- Mirror/mirrorNetwork.py
+++ Mirror/mirrorNetwork.py
@@ -4,95 +4,104 @@ import _thread
 
 
 def networkInit(recvQueue, sendQueue):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	localAddress = ('localhost', 0)
-	s.bind(localAddress)
-	_thread.start_new_thread(mirrorNetRecv, (s,recvQueue,))
-	_thread.start_new_thread(mirrorNetSend, (s,recvQueue,))
-	
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        localAddress = ('', 0)
+        _thread.start_new_thread(mirrorNetRecv, (s,recvQueue,))
+        _thread.start_new_thread(mirrorNetSend, (s,sendQueue,))
+    
 
 def mirrorNetRecv(s, queue):
-	while True:
-		buf, address = s.recvfrom(port)
-		if not len(buf):
-			break
-		stringdata = buf.decode('utf-8')
-		if(stringdata[:2] == '01'):
-			queue.put_nowait(message('id', stringdata[3:]))
-		elif(stringdata[:2] == '00'):
-			queue.put_nowait(message('beat', ''))
-		elif(stringdata[:2] == '02'):
-			queue.put_nowait(message('data', stringdata[3:]))
-		else:
-			recvError = open('/logs/RecvError.log', 'a')
-			recvError.write(stringdata)
-			recvError.close()
-	s.shutdown(1)
-	
+        while True:
+                buf, address = s.recvfrom(3010)
+                stringdata = buf.decode('utf-8')
+                print(stringdata)
+                if(stringdata[:2] == '01'):
+                        print('eloo')
+                        queue.put_nowait(message('id', stringdata[3:]))
+                elif(stringdata[:2] == '00'):
+                        queue.put_nowait(message('beat', ''))
+                elif(stringdata[:2] == '02'):
+                        queue.put_nowait(message('data', stringdata[3:]))
+                else:
+                        recvError = open('RecvError.log', 'a')
+                        recvError.write(stringdata)
+                        recvError.close()
+        s.shutdown(1)
+    
 
 def mirrorNetSend(s, queue):
-	serverIp = '10.0.0.1'
-	servPort = 3010
-	server_address = (serverIp, servPort)
-	heartBeat(s,'-1')
-	while True:
-		if not queue.empty():
-				while not queue.empty():
-					messageToSend = queue.get()
-					if(messageToSend.type == 'beat'):
-						heartBeat(s,messageToSend.info)
-					elif(messageToSend.type == 'data'):
-						data = '22/' + messageToSend.info
-						s.sendto(data.encode('utf-8'), server_address)
-					elif(messageToSend.type == 'ack'):
-						sendAck(s,messageToSend.info)
-					else:
-						sendError = open('/logs/sendError.log', 'a')
-						sendError.write(stringdata)
-						sendError.close()
-	s.shutdown(1)
+        serverIp = '10.0.0.1'
+        servPort = 3010
+        server_address = (serverIp, servPort)
+        heartBeat(s,'-1')
+        print('outloop')
+        while True:
+                if not queue.empty():
+                        while not queue.empty():
+                                messageToSend = queue.get()
+                                if(messageToSend.messageType == 'beat'):
+                                        print(messageToSend.info)
+                                        heartBeat(s,messageToSend.info)
+                                elif(messageToSend.messageType == 'data'):
+                                        data = '22/' + messageToSend.info
+                                        print('Sending: ' + data)
+                                        s.sendto(data.encode('utf-8'), server_address)
+                                elif(messageToSend.messageType == 'ack'):
+                                        sendAck(s,messageToSend.info)
+                                else:
+                                        sendError = open('sendError.log', 'a')
+                                        sendError.write(messageToSend.messageType + ':' + messageToSend.info + '\n')
+                                        sendError.close()
+        s.shutdown(1)
 
 
 def heartBeat(s,id):
-	data = '20/' + id + '/2'
-	s.sendto(data.encode('utf-8'), server_address)
+        host = '10.0.0.1'
+        port = 3010
+        server_address = (host, port)
+        data = '20/' + id + '/2'
+        print('Sending: ' + data)
+        s.sendto(data.encode('utf-8'), server_address)
 
 def sendAck(s,id):
-	data = '21/' + id + '/2'
-	s.sendto(data.encode('utf-8'), server_address)
+        host = '10.0.0.1'
+        port = 3010
+        data = '21/' + id + '/2'
+        print('Sending: ' + data)
+        s.sendto(data.encode('utf-8'), server_address)
 
 def sendFakeData ():
-	host = '127.0.0.1'
+    host = '127.0.0.1'
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	port = 8080
-	server_address = (host, port)
-	timeBetweenSends = 2
-	while 1:
-		data ='c' + '#FF0000'
-		s.sendto(data.encode('utf-8'), server_address)	
-		time.sleep(timeBetweenSends)
-		
-		weather = open('weatherTest.txt', 'r')
-		data = weather.read()
-		weather.close()
-		data ='w' + data
-		s.sendto(data.encode('utf-8'), server_address)
-		time.sleep(timeBetweenSends)
-		
-		data ='c' + '#00FF00'
-		s.sendto(data.encode('utf-8'), server_address)	
-		time.sleep(timeBetweenSends)
-		
-		bus = open('busTest.txt', 'r')
-		data = bus.read()
-		bus.close()
-		data ='b' + data
-		s.sendto(data.encode('utf-8'), server_address)	
-		time.sleep(timeBetweenSends)
-		
-		data ='c' + '#2E99A9'
-		s.sendto(data.encode('utf-8'), server_address)	
-		time.sleep(timeBetweenSends)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    port = 8080
+    server_address = (host, port)
+    timeBetweenSends = 2
+    while 1:
+        data ='c' + '#FF0000'
+        s.sendto(data.encode('utf-8'), server_address)    
+        time.sleep(timeBetweenSends)
+        
+        weather = open('weatherTest.txt', 'r')
+        data = weather.read()
+        weather.close()
+        data ='w' + data
+        s.sendto(data.encode('utf-8'), server_address)
+        time.sleep(timeBetweenSends)
+        
+        data ='c' + '#00FF00'
+        s.sendto(data.encode('utf-8'), server_address)    
+        time.sleep(timeBetweenSends)
+        
+        bus = open('busTest.txt', 'r')
+        data = bus.read()
+        bus.close()
+        data ='b' + data
+        s.sendto(data.encode('utf-8'), server_address)    
+        time.sleep(timeBetweenSends)
+        
+        data ='c' + '#2E99A9'
+        s.sendto(data.encode('utf-8'), server_address)    
+        time.sleep(timeBetweenSends)
 
-	s.shutdown(1)
+    s.shutdown(1)
