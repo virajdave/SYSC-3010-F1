7895a4591ea088adbc9c8ed069f4862fedb6dd4f
Dillon Verhaeghe <nollid30@hotmail.com>
2017-04-03 20:21:15 -0400

    finished added recved messages testing for data messages

--- Mirror/mirror_test.py
+++ Mirror/mirror_test.py
@@ -22,7 +22,7 @@ def test_guiUpdateTime():
 	time.sleep(0.01)
 	stop()
 	returned = testQueue.get_nowait()
-	assert returned.messageType == 'time', "time update did not have type time"
+	assert (returned.messageType == 'time'), "time update did not have type time"
 
 def test_updateWeather():
 	testQueue = Queue()
@@ -32,8 +32,8 @@ def test_updateWeather():
 	time.sleep(1)
 	stop()
 	returned = testQueue.get_nowait()
-	assert returned.messageType == 'data', "Weather update did not have type data"
-	assert returned.info == '10/weather', "Weather update did not have info 10/weather"
+	assert (returned.messageType == 'data'), "Weather update did not have type data"
+	assert (returned.info == '10/weather'), "Weather update did not have info 10/weather"
 	
 def test_updateBusInfo():
 	testQueue = Queue()
@@ -43,8 +43,8 @@ def test_updateBusInfo():
 	time.sleep(1)
 	stop()
 	returned = testQueue.get_nowait()
-	assert returned.messageType == 'data', "Bus update did not have type data"
-	assert returned.info == '10/bus', "Bus update did not have info 10/bus"
+	assert (returned.messageType == 'data'), "Bus update did not have type data"
+	assert (returned.info == '10/bus'), "Bus update did not have info 10/bus"
 	
 	
 def test_timeSync():
@@ -55,9 +55,65 @@ def test_timeSync():
 	time.sleep(1)
 	stop()
 	returned = testQueue.get_nowait()
-	assert returned.messageType == 'data', "Sync  did not have type data"
-	assert returned.info == '10/time', "Sync  did not have info 10/time"
+	assert (returned.messageType == 'data'), "Sync  did not have type data"
+	assert (returned.info == '10/time'), "Sync  did not have info 10/time"
 
+def test_RecvMessages_weather():
+    testRecv = Queue()
+    testSend = Queue()
+    testgui = Queue()
+    run()
+    _thread.start_new_thread(watchRecvMessages, (testRecv,testSend,testgui,))
+    weatherDataFile = open('testData/weatherTest.txt', 'r')
+    weatherData = weatherDataFile.read()
+    testRecv.put_nowait(message('data', 'w' + weatherData))
+    time.sleep(1)
+    stop()
+    returned = testgui.get_nowait()
+    expected = data_organizer(weatherData)
+    assert (returned.messageType == 'weather'), 'Recved message did not give message of type weather back'
+    assert (set(returned.info.keys()) == set(expected.keys())), 'Recved messaged did not give the correct weather data back'
     
+def test_RecvMessages_bus():
+    testRecv = Queue()
+    testSend = Queue()
+    testgui = Queue()
+    run()
+    _thread.start_new_thread(watchRecvMessages, (testRecv,testSend,testgui,))
+    busDataFile = open('testData/busTest.txt', 'r')
+    busData = busDataFile.read()
+    testRecv.put_nowait(message('data', 'b' + busData))
+    time.sleep(1)
+    stop()
+    returned = testgui.get_nowait()
+    expected = parseBusInfo(busData)
+    assert (returned.messageType == 'bus'), 'Recved message did not give message of type bus back'
+    assert (set(returned.info.keys()) == set(expected.keys())), 'Recved messaged did not give the correct weather data back'
 
+def test_RecvMessages_colour():
+    testRecv = Queue()
+    testSend = Queue()
+    testgui = Queue()
+    run()
+    _thread.start_new_thread(watchRecvMessages, (testRecv,testSend,testgui,))
+    colour = '#FFFFFF'
+    testRecv.put_nowait(message('data', 'c' + colour))
+    time.sleep(1)
+    stop()
+    returned = testgui.get_nowait()
+    assert (returned.messageType == 'colour'), 'Recved message did not give message of type colour back'
+    assert (returned.info == colour), 'Recved messaged did not give the correct colour data back'
 
+def test_RecvMessages_direction():
+    testRecv = Queue()
+    testSend = Queue()
+    testgui = Queue()
+    run()
+    _thread.start_new_thread(watchRecvMessages, (testRecv,testSend,testgui,))
+    direct = '1'
+    testRecv.put_nowait(message('data', 'd' + direct))
+    time.sleep(1)
+    stop()
+    returned = testgui.get_nowait()
+    assert (returned.messageType == 'direction'), 'Recved message did not give message of type direction back'
+    assert (returned.info == direct), 'Recved messaged did not give the correct direction data back'    
