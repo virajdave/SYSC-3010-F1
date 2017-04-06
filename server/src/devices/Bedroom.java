79d2f4bbee509d93c516c5172fd8d91dd2702687
Conrodot <connor.emery@yahoo.com>
2017-04-06 18:14:06 -0400

    Update Bedroom.java
    Adding getTime

--- server/src/devices/Bedroom.java
+++ server/src/devices/Bedroom.java
@@ -7,9 +7,14 @@ public class Bedroom extends Device {
 	boolean lights;
 	@Override
 	public void giveMessage(String msg) {
-		
+		if (msg.equals("time")) {
+        	    dataOut = getTime();
+		}
 	}
-
+	public String getTime() {
+        	long time = System.currentTimeMillis();
+        	return "t"+ time;
+    }
 	@Override
 	public boolean giveInput(Data in) {
 		send(in.getName() + "/" + in.get());
