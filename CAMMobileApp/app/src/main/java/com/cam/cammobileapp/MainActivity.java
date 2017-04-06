2127cb1aa904b68abfe6d4671dec823f8194822e
virajdave <virajdave@cmail.carleton.ca>
2017-04-06 00:32:34 -0400

    Got the ID's of the devices into an ArrayList

--- CAMMobileApp/app/src/main/java/com/cam/cammobileapp/MainActivity.java
+++ CAMMobileApp/app/src/main/java/com/cam/cammobileapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cam.cammobileapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,26 +10,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    final Context prev = this;
     public static ServerOnApp server = new ServerOnApp();
-
+    public static ParseThermo parsedThermostat = new ParseThermo();
+    private ArrayList<Integer> id_thermo = new ArrayList<>();
+    //public static ParseAlarm parsedAlarms = new ParseAlarms();
+    //public static ParseMirror parsedMirrors = new ParseMirrors();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         server.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         this.sendMessage();
 
 
-    ImageButton imageButton1 = (ImageButton) findViewById(R.id.btn_thermostat);
-    imageButton1.setOnClickListener(new View.OnClickListener(){
+        ImageButton imageButton1 = (ImageButton) findViewById(R.id.btn_thermostat);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
 
-        public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, SecondMainActivity.class);
-        startActivity(intent);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondMainActivity.class);
+                startActivity(intent);
             /*
             Fill in code to show available thermostats
              */
@@ -38,8 +49,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         ImageButton imageButton2 = (ImageButton) findViewById(R.id.btn_magicMirror);
-        imageButton2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent intent2 = new Intent(MainActivity.this, ThirdMainActivity.class);
                 startActivity(intent2);
                 /*
@@ -75,8 +86,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void sendMessage(){
-        String message = "10";
-            server.sendBroadcast(message);
+    public void sendMessage() {
+        //String message = "10";
+        //server.sendBroadcast(message);
+        //String s = server.recvWait(1000);
+        String s = "00/2:3:0/6:2:1/7:2:1/8:2:0";
+        if (s == null) {
+            Toast.makeText(prev, "Message is null, server not connected", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(prev, s, Toast.LENGTH_LONG).show();
+            id_thermo = parsedThermostat.parseString(s);
+            String result ="";
+            for(int top: id_thermo){
+                result += top;
+            }
+            Toast.makeText(prev, result, Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ CAMMobileApp/app/src/main/java/com/cam/cammobileapp/ParseThermo.java
@@ -0,0 +1,80 @@
+package com.cam.cammobileapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.EditText;
+import java.util.*;
+
+/**
+ * Created by virajdave on 2017-04-05.
+ */
+
+public class ParseThermo extends AppCompatActivity {
+
+    final Context prev = this;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thermos_available);
+    }
+
+    String message = "";
+
+    public ParseThermo() {
+    }
+
+
+    public ArrayList<Integer> parseString(String message) {
+
+
+        System.out.println("helllo");
+        ArrayList<Integer> id_thermo = new ArrayList<>();
+        String parsingColon[] = new String[10000];
+
+        if (message.substring(0, 2).equals("00")) {
+
+            String parsed[] = message.substring(3).split("/");
+            System.out.println("");
+            for (int i = 0; i < 4; i++) {
+                System.out.println(parsed[i]);
+            }
+            for (int i = 0; i < 4; i++) {
+                char placeHolder = parsed[i].charAt(2);
+                System.out.println(placeHolder);
+
+
+                if (placeHolder == '2') {
+                    char addedId = parsed[i].charAt(0);
+                    System.out.println(addedId);
+                    System.out.println("");
+
+
+                    //System.out.println(addedId);
+                    id_thermo.add(Character.getNumericValue(addedId));
+
+                } else {
+                    System.out.println("Nope not this device");
+                    System.out.println("");
+                }
+
+            }
+
+
+        }
+        System.out.println("");
+        System.out.println("In ArrayList");
+        System.out.println("");
+        for (int value : id_thermo) {
+
+            System.out.println(value);
+
+        }
+        return id_thermo;
+    }
+}
