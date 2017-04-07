12b3277eec32e3bab4db9e9679f75726d29fcfa8
bencroskery <ben@irtech.com>
2017-04-07 05:23:51 -0400

    Working Mirror colour and location

--- CAMMobileApp/app/src/main/java/com/cam/cammobileapp/MirrorActivity.java
+++ CAMMobileApp/app/src/main/java/com/cam/cammobileapp/MirrorActivity.java
@@ -1,6 +1,7 @@
 package com.cam.cammobileapp;
 
 import android.app.Activity;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,28 +17,25 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
+import com.cam.cammobileapp.util.Parse;
 import com.cam.cammobileapp.util.Toasty;
 import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+
 import android.util.Log;
 
 public class MirrorActivity extends AppCompatActivity {
     final Activity activity = this;
 
-    TextView theLatCoord, theLongCoord;
-    public LocationManager locationManager;
-    public double longitude;
-    public double latitude;
+    private LocationManager locationManager;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_mirror);
 
-
         Intent intent = getIntent();
+        final int id = intent.getIntExtra("deviceID", -1);
 
-        final ImageButton imageButton5 = (ImageButton) findViewById(R.id.btn_setTrans);
-        imageButton5.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_setTrans).setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
@@ -67,42 +65,41 @@ public class MirrorActivity extends AppCompatActivity {
             }
         });
 
-
-        final ImageButton imageButton7 = (ImageButton) findViewById(R.id.btn_colour);
-        imageButton7.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_colour).setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-
-                final ColorPicker cp = new ColorPicker(MirrorActivity.this, 0, 0, 0);
+                final ColorPicker cp = new ColorPicker(activity, 0, 0, 0);
                 cp.show();
-                Button colourConfirmed = (Button) cp.findViewById(R.id.okColorButton);
-                colourConfirmed.setOnClickListener(new View.OnClickListener() {
+                cp.findViewById(R.id.okColorButton).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        int redRGB = cp.getRed();
-                        int greenRGB = cp.getGreen();
-                        int blueRGB = cp.getBlue();
-                        int theRGBCode = redRGB+greenRGB+blueRGB;
-                        String inString = Integer.toString(theRGBCode);
-                        String HexRed = Integer.toHexString(redRGB);
-                        String HexGreen = Integer.toHexString(greenRGB);
-                        String HexBlue = Integer.toHexString(blueRGB);
-                        String finalRGB = "#"+HexRed+HexGreen+HexBlue;
-
-                        //10/id/colour/(Colourcode)
-                        // id(must get it with the a list of devices)
-
                         cp.dismiss();
-                        String messageForColour = "12/id/colour/" + finalRGB;
-                        Toasty.show(activity, finalRGB);
+
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Convert RGB to a hex colour value.
+                                String HexRed = Integer.toHexString(cp.getRed());
+                                String HexGreen = Integer.toHexString(cp.getGreen());
+                                String HexBlue = Integer.toHexString(cp.getBlue());
+                                String finalRGB = "#" + HexRed + HexGreen + HexBlue;
+
+                                // Send colour to server and check it worked.
+                                MainActivity.server.sendBroadcast(Parse.toString("/", "12", id, "colour", finalRGB));
+                                String msg = MainActivity.server.recvWait(1000);
+                                if (msg != null && msg.charAt(3) == '1') {
+                                    Toasty.show(activity, "Set the mirror colour to " + finalRGB);
+                                } else {
+                                    Toasty.show(activity, "Could not set the mirror colour");
+                                }
+                            }
+                        }).start();
                     }
                 });
             }
         });
 
-
-        final ImageButton imageButton6 = (ImageButton) findViewById(R.id.btn_setLocation);
-        imageButton6.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_setLocation).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -110,87 +107,74 @@ public class MirrorActivity extends AppCompatActivity {
                 final View location_layout = getLayoutInflater().inflate(R.layout.location_layout, null);
                 locationDialog.setView(location_layout);
                 locationDialog.show();
-                Button enableLocation = (Button) locationDialog.findViewById(R.id.turnOnGPS);
-                Button sendLocation = (Button) locationDialog.findViewById(R.id.sendGPS);
 
-                theLatCoord = (TextView) location_layout.findViewById(R.id.latCoord);
-                theLongCoord = (TextView) location_layout.findViewById(R.id.longCoord);
+                final TextView theLatCoord = (TextView) location_layout.findViewById(R.id.latCoord);
+                final TextView theLongCoord = (TextView) location_layout.findViewById(R.id.longCoord);
 
-
-                enableLocation.setOnClickListener(new View.OnClickListener() {
+                new Thread(new Runnable() {
                     @Override
-                    public void onClick(View v){
+                    public void run() {
+                        try {
+                            Looper.prepare();
+                            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                            final LocationListener listenerForLocation = new LocationListener() {
+                                @Override
+                                public void onLocationChanged(final Location location) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            theLatCoord.setText(Double.toString(location.getLatitude()));
+                                            theLongCoord.setText(Double.toString(location.getLongitude()));
+                                        }
+                                    });
+                                }
 
-                        try{
-                        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                        final LocationListener listenerForLocation = new ListenerForLocation();
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listenerForLocation);
-                        theLatCoord = (TextView) location_layout.findViewById(R.id.latCoord);
-                        theLongCoord = (TextView) location_layout.findViewById(R.id.longCoord);
-                        Location theCoord = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        longitude = theCoord.getLongitude();
-                        latitude = theCoord.getLatitude();
-                            String finalLongitude = Double.toString(longitude);
-                            String finalLatitude = Double.toString(latitude);
-                            Log.e("Here", finalLatitude+finalLongitude);
-                            theLatCoord.setText(finalLatitude);
-                            theLongCoord.setText(finalLongitude);
+                                @Override
+                                public void onProviderDisabled(String s) {
+                                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                }
+
+                                @Override
+                                public void onStatusChanged(String s, int i, Bundle bundle) {}
+                                @Override
+                                public void onProviderEnabled(String s) {}
+                            };
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listenerForLocation);
+                            final Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    theLatCoord.setText(Double.toString(location.getLatitude()));
+                                    theLongCoord.setText(Double.toString(location.getLongitude()));
+                                }
+                            });
+                        } catch (Exception ce) {
+                            locationDialog.dismiss();
+                            Toasty.show(activity, "Could not lock GPS location");
+                            Log.e("here", "ce", ce);
+                        }
                     }
+                }).start();
 
-                    catch (Exception ce){
-                        Log.e("Here", "This", ce);
-                    }
-
+                locationDialog.findViewById(R.id.sendGPS).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                MainActivity.server.sendBroadcast(Parse.toString("/", "12", id, "loc", theLatCoord.getText() + "," + theLongCoord.getText()));
+                                String msg = MainActivity.server.recvWait(1000);
+                                if (msg != null && msg.charAt(3) == '1') {
+                                    Toasty.show(activity, "Successfully set Magic Mirror location to " + theLatCoord.getText() + ", " + theLongCoord.getText());
+                                } else {
+                                    Toasty.show(activity, "Unable to send location to Magic Mirror");
+                                }
+                            }
+                        }).start();
                     }
                 });
-
-                final String finalLong = theLongCoord.getText().toString();
-                final String finalLat = theLatCoord.getText().toString();
-                sendLocation.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String locationToMirror = "12/id/loc/" + finalLong + "," + finalLat;
-                        locationDialog.dismiss();
-                        Toasty.show(activity, "Location sent to Magic Mirror");
-                    }
-                });
-
-
-
             }
         });
-    }
-
-    class ListenerForLocation implements LocationListener {
-        //TextView theLatCoord, theLongCoord;
-
-        @Override
-        public void onLocationChanged(Location location) {
-            // TODO Auto-generated method stub
-            theLatCoord.setText(Double.toString(location.getLatitude()));
-            theLongCoord.setText(Double.toString(location.getLongitude()));
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            // TODO Auto-generated method stub
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            // TODO Auto-generated method stub
-
-        }
     }
 }
 
