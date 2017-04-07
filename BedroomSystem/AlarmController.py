4dcd1a88e867fb25b32ef91f57dce7a82698f5c7
Conrodot <connor.emery@yahoo.com>
2017-04-07 15:15:42 -0400

    Update AlarmController.py
    Final Version Alarm Controller

--- BedroomSystem/AlarmController.py
+++ BedroomSystem/AlarmController.py
@@ -7,12 +7,28 @@ from threading import Timer
 import RPi.GPIO as GPIO
 import socket, sys
 
+#takes as input number of ms since 1970 getting info from server
+#sets the current system time to the value from server
+def linux_set_time(milliSeconds):
+    formatedTime = datetime.datetime.fromtimestamp(int(milliSeconds)/1000)
+    time_tuple = (formatedTime.year, formatedTime.month,formatedTime.day,formatedTime.hour,formatedTime.minute,formatedTime.second,formatedTime.microsecond)
+    systemCall = 'sudo date -s "'
+    systemCall += str(formatedTime.day)
+    systemCall += ' ' + formatedTime.strftime('%B')
+    systemCall += ' ' + str(formatedTime.year)
+    systemCall += ' ' + str(formatedTime.hour) + ':'
+    systemCall += str(formatedTime.minute) + ':'
+    systemCall += str(formatedTime.second) + '"'
+    os.system(systemCall)
+
 class AlarmController:
     alarm = AlarmSystem()
+	#invalid default id
     id = -1
     t = Timer
 
      #starts new alarm/ disables any pre-existing alarms
+	 #checks if new alarm time is valid if not the alarm will not be set
     def newAlarm (self, data):
         if (self.alarm.alarmOn):
             print ('disabling old alarm')
@@ -22,11 +38,10 @@ class AlarmController:
         if ( validTime(data) == False):
             print('alarm not valid')
             return
-        dataH = data[4:]
-        #print ('data = '+ data)
+			#pulling hour, minute info out of message string from server
+        dataH = data[9:]
         dataH = dataH[:2]
-        #print ('data h = ' + dataH)
-        dataM = data[7:]
+        dataM = data[12:]
         
         h = int (dataH)
         m = int (dataM)
@@ -36,7 +51,7 @@ class AlarmController:
         self.t = Timer(seconds, self.alarm.manageAlarm, )
         self.t.start()
         self.alarm.alarmOn = True
-            #sleep(5)
+		
     def runSystem(self):
         #creating datagram , sending heartbeat (setup)
         s = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
@@ -55,28 +70,32 @@ class AlarmController:
 
         #   Wait for intructions from server
             buf, address = s.recvfrom(3010)
-            print(buf)
             stringData = buf.decode('utf-8')
-            
-            print('got: '+ stringData)
+            #decode message to instruction
+			#01 server ack comes with device id
             if (stringData[:2] == '01'):
-                print("id = "+ stringData[3:])
+                
                 if (self.id == -1):
                     self.id = stringData[3:]
+                    print("id = "+ str(self.id))
+                continue
+				#heartbeat respond with heartbeat message
             elif (stringData[:2] == '00'):
                 print("heartbeat response")
                 message = '20/' + str(self.id)+ '/4'
                 s.sendto(message.encode('utf-8'), server_address)
+				#data from server decode if lights, time sync, or alarm
             elif (stringData[:2] == '02'):
                     print("data received")
                     sd = stringData[3:]
+                    print (sd)
                     print('data = ' + sd)
                     if (sd[:1] == 't'):
-                        linux_set_time(sd[1:])
-                    elif:(sd[:1] == 'lo'):
+                        linux_set_time (sd[1:])
+                    elif(sd == 'l/1'):
                         self.alarm.lights(True)
-                    elif:(sd[:1] == 'lf'):
-                        self.alarm.lights(True)
+                    elif(sd == 'l/0'):
+                        self.alarm.lights(False)
                     else:
                         self.newAlarm(stringData)
             #sync exists to setup request for server to get its current time
@@ -86,53 +105,33 @@ class AlarmController:
                 print ('sending time sync')
                 sync = 0
                 message = '22/' + self.id + '/time'
+                s.sendto(message.encode('utf-8'), server_address)            
+            #updates the server on the current status of the lights
+            if (self.alarm.lightsOn == True): 
+                message = '22/' + self.id + '/LO'
                 s.sendto(message.encode('utf-8'), server_address)
-            #for testing to be removed
-            h = datetime.datetime.now().hour
-            m = datetime.datetime.now().minute + 1
-            stringDataT = '02/a'+ str(h) +","+ str(m)
-            if (self.alarm.alarmOn == False):
-                print('Data T= '+stringDataT)
-                self.newAlarm(stringDataT)
+            if (self.alarm.lightsOn == False): 
+                message = '22/' + self.id + '/LF'
+                s.sendto(message.encode('utf-8'), server_address)
             
-        # for now assuming alarm message with contents
-            self.alarm.lights(True)
-            sleep(1)
-            self.alarm.lights(False)
-            #end of testing
-            
-
+#compares alarm time vs current time alarms cannot be set in the past
 def validTime(data):
     
     h = datetime.datetime.now().hour
     m = datetime.datetime.now().minute
-    #print ("data " + data[3:])
-    dataH = data[4:]
-    #print ('l'+data)
+    dataH = data[9:]
     dataH = dataH[:2]
-    
-    #print ('data h = ' + dataH)
-    dataM = data[7:]
-    #print ('dataM' + dataM)
-    if (int(dataH) >= h -12 ):
+
+    dataM = data[12:]
+
+    if (int(dataH) >= h ):
         if (int(dataM) >= m):
-            #print('alarm Valid')
+
             return True
     return False
+#start up
 ac = AlarmController()
 ac.runSystem()
 
-#takes as input number of ms since 1970 getting info from server
-def linux_set_time(milliSeconds):
-    formatedTime = datetime.datetime.fromtimestamp(int(milliSeconds)/1000)
-    time_tuple = (formatedTime.year, formatedTime.month,formatedTime.day,formatedTime.hour,formatedTime.minute,formatedTime.second,formatedTime.microsecond)
-    systemCall = 'sudo date -s "'
-    systemCall += str(formatedTime.day)
-    systemCall += ' ' + formatedTime.strftime('%B')
-    systemCall += ' ' + str(formatedTime.year)
-    systemCall += ' ' + str(formatedTime.hour) + ':'
-    systemCall += str(formatedTime.minute) + ':'
-    systemCall += str(formatedTime.second) + '"'
-    os.system(systemCall)
 
 
