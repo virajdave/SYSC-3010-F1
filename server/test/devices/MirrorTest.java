44f4b1eb6c079288f2269ef6295a278fc9e60c0e
Ben Croskery <ben@irtech.com>
2017-04-06 22:32:36 -0400

    Fixing tests
    - things were failing due to web/db calls

--- server/test/devices/MirrorTest.java
+++ server/test/devices/MirrorTest.java
@@ -5,30 +5,37 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 
 import types.Data;
+import types.Web;
+import util.DatabaseStub;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class MirrorTest {
+	Device test;
+	
+	@Before
+	public void setup() {
+		test = Device.createNew(2, 7, new Web(new DatabaseStub()));
+	}
 
 	@Test
 	public void testgetColour() {
-		Mirror m = new Mirror();
-		assertEquals("#2E99A9", m.requestOutput(new Data("colour")).get());
+		assertEquals("#2E99A9", test.requestOutput(new Data("colour")).get());
 	}
 	
 	@Test
 	public void testsetColourpass() {
-		Mirror m = new Mirror();
 		String newColour = "#FFFFFF";
-		m.giveInput(new Data("colour",newColour));
-		assertEquals(newColour, m.requestOutput(new Data("colour")).get());
+		test.giveInput(new Data("colour",newColour));
+		assertEquals(newColour, test.requestOutput(new Data("colour")).get());
 	}
 	
 	@Test
 	public void testsetColourFail() {
-		Mirror m = new Mirror();
 		String newColour = "#FFFFFFFFFFFFFFFF";
-		m.giveInput(new Data("colour",newColour));
-		assertNotEquals(newColour, m.requestOutput(new Data("colour")).get());
+		test.giveInput(new Data("colour",newColour));
+		assertNotEquals(newColour, test.requestOutput(new Data("colour")).get());
 	}
 	
 }
--- server/test/devices/SwitchTest.java
+++ server/test/devices/SwitchTest.java
@@ -2,33 +2,36 @@ package devices;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import types.Data;
+import types.Web;
+import util.DatabaseStub;
 
 public class SwitchTest {
+	Device test;
+	
+	@Before
+	public void setup() {
+		test = Device.createNew(1, 3, new Web(new DatabaseStub()));
+	}
 
 	@Test
 	public void testCreate() {
-		Device test = Device.createNew(1, 0, null);
-		
 		assertTrue(test instanceof Switch);
-		assertEquals(0, test.getID());
-		assertTrue(test.hasID(0));
+		assertEquals(3, test.getID());
+		assertTrue(test.hasID(3));
 		assertFalse(test.hasID(50));
 	}
 	
 	@Test
 	public void testInfo() {
-		Device test = new Switch();
-		assertEquals("-1/0/0/-1", test.getInfo());
-		
-		test = Device.createNew(1, 5, null);
-		assertEquals("5/0/0/-1", test.getInfo());
+		assertEquals("3/0/0/-1", test.getInfo());
 		test.setDead(true);
-		assertEquals("5/1/0/-1", test.getInfo());
+		assertEquals("3/1/0/-1", test.getInfo());
 		test.giveInput(new Data("set", "1"));
-		assertEquals("5/1/1/-1", test.getInfo());
+		assertEquals("3/1/1/-1", test.getInfo());
 	}
 	
 }
